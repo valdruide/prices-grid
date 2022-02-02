5cd28c322f6212a7d55c55eb9193343dde45f406
Valdruide <tviardprimo190@gmail.com>
2022-02-02 12:40:27 +0100

    push

---
+++ style/JS/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+});
+
+
+
+
+
+
+// Accordéon Services
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+    
+  });
+}
---
+++ style/JS/parallax.js
@@ -0,0 +1,7 @@
+
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2);
